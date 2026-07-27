956eb7502593f0c77ee86912d4700931593726e7
Vincent Gichuki <150811208+vinxy254@users.noreply.github.com>
2026-07-27 20:25:33 +0300

    Add Citizen Xtra channel to channels.js

--- web/smart-tv/js/channels.js
+++ web/smart-tv/js/channels.js
@@ -22,6 +22,16 @@ const channels = [
     infoPage: "N/A"
   },
   {
+    name: "Citizen Xtra",
+    type: "m3u8",
+    streamUrl: "https://live-onesignal.royalmedia.co.ke/live/jgB35emfPsiW5aFjKJjbjg.m3u8",
+    videoId: "gzw1hoiBK6Q",
+    logo: "https://static.wikia.nocookie.net/logopedia/images/7/70/Citizen_TV_%28Kenya%29_logo.png",
+    category: "National",
+    Rating: "N/A",
+    infoPage: "N/A"
+  },
+  {
     name: "KTN Home",
     type: "youtube",
     channelUrl: "UCKVsdeoHExltrWMuK0hOWmg",
